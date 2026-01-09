5e50543811eec61a3f7472731df9b02a33b7a9a5
Daniel Antonio <dantoniorh92@outlook.com>
2026-01-08 20:30:43 -0400

    Tarjeta 4: Maquetacion base con CSS y render_template

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-    # Return es lo que el servidor le envia al navegador
-    return "<h1>Servidor de Finanzas Activo</h1><p>Hola Daniel, el entorno funciona.</p>"
+    # Busca archivo "index.html" en templates
+    return render_template('index.html')
 if __name__ == '__main__':
     # debug=True permite que el servidor se reinicie solo cuando haga cambios en el codigo
     app.run(debug=True)
